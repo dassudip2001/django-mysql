e7d4855eac63bf15114d06773618fefd87e2a270
Sudip Das <85440350+dassudip2001@users.noreply.github.com>
2022-06-16 19:55:46 +0530

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'djongo',
@@ -92,11 +92,11 @@ DATABASES = {
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'test',
-#         'USER': 'admin',
-#         'PASSWORD': 'Admin@123',
+#         'NAME': 'db',
+#         'USER': 'user',
+#         'PASSWORD': 'password',
 #         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'PORT': '3360',
 #     }
 # }
 
